edaaae2bfdcb21d0e50eb8be40afb07b9857fcbd
Vladislav Sidorov <foreignforger@gmail.com>
2023-12-04 00:54:48 +0400

    Updated settings

--- FileSignatures/FileSignaturesConsoleApp/Settings/Settings.cs
+++ FileSignatures/FileSignaturesConsoleApp/Settings/Settings.cs
@@ -2,23 +2,21 @@
 {
     public class Settings
     {
-        public String FilePath { get; set; }
+        public String FilePath { get; }
+        public Int32 SegmentSize { get; }
+        public Int32 WorkThreadsCount { get; }
+        public QueueSettings QueueSettings { get; }
+        public Int32? LogBufferSize { get; }
+        public Boolean DoCountDown { get; }
 
-        public Int32 SegmentSize { get; set; }
-
-        public Int32? LogBufferSize { get; set; }
-
-        public Int32 WorkThreadsCount { get; set; }
-
-        public QueueSettings QueueSettings { get; set; }
-
-        public Boolean DoCountDown { get; set; }
-
-        public Settings() 
+        public Settings(String filePath, Int32 segmentSize, Int32 workThreadsCount, QueueSettings queueSettings, Int32? logBufferSize, Boolean doCountDown) 
         {
-            QueueSettings = new QueueSettings();
-            LogBufferSize = null;
-            DoCountDown = false;
+            FilePath = filePath;
+            SegmentSize = segmentSize;
+            WorkThreadsCount = workThreadsCount;
+            QueueSettings = queueSettings;
+            DoCountDown = doCountDown;
+            LogBufferSize = logBufferSize;
         }
     }
 }
--- FileSignatures/FileSignaturesConsoleApp/Settings/SettingsManager.cs
+++ FileSignatures/FileSignaturesConsoleApp/Settings/SettingsManager.cs
@@ -11,55 +11,44 @@
         //TODO better to use some library for arguments or at least have arguments names
         public Settings GetSettings(string[] args)
         {
-            Settings settings = new Settings();
-
             if (args.Length < 2)
-            {
                 throw new ArgumentException("Not enough arguments provided. Please set file path and segment size.");
-            }
 
-            settings.FilePath = args[0];
+            String filePath = args[0];
+
             Int32 segmentSize = Int32.Parse(args[1]);
-
-            if (segmentSize > 0)
-            {
-                settings.SegmentSize = segmentSize;
-            }
-            else
-            {
+            if (segmentSize <= 0)
                 throw new ArgumentException("Incorrect segment size format!. Segment size should be a positive integer.");
-            }
 
+            Int32? logBufferSize = null;
             if (args.Length >= 3)
             {
-                Int32 logBufferSize = Int32.Parse(args[2]);
-                if (logBufferSize > 0)
-                {
-                    settings.LogBufferSize = logBufferSize;
-                }
-
+                logBufferSize = Int32.Parse(args[2]);
+                if (logBufferSize <= 0)
+                    logBufferSize = null;
             }
 
+            QueueSettings queueSettings = new QueueSettings();
             if (args.Length >= 5)
             {
                 Int32 queueCooldownTime = Int32.Parse(args[3]);
                 Int32 maxQueueSize = Int32.Parse(args[4]);
 
                 if (queueCooldownTime <= 0 && maxQueueSize > 0)
-                {
                     _logger.LogWarning("MaxQueueSize parameter doesn't affect the application if Cooldowntime parameter is zero/turned down");
-                }
 
                 if (queueCooldownTime > 0 && maxQueueSize <= 0)
-                {
                     _logger.LogWarning("Cooldowntime parameter doesn't affect the application if MaxQueueSize parameter is zero/turned down");
-                }
 
-                settings.QueueSettings = new QueueSettings(maxQueueSize, queueCooldownTime);
+                queueSettings = new QueueSettings(maxQueueSize, queueCooldownTime);
             }
 
-            settings.WorkThreadsCount = Environment.ProcessorCount;
+            Boolean doCountDown = false;
+            if(args.Length >= 6)
+                doCountDown = Boolean.Parse(args[5]);
 
+            Int32 workThreadsCount = Environment.ProcessorCount;
+            Settings settings = new Settings(filePath, segmentSize, workThreadsCount, queueSettings, logBufferSize, doCountDown);
             return settings;
         }
 
